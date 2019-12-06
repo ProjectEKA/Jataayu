0d4a0602775f65298b88c0e8ae51c9abac7d6fad
Bande Rajkiran <bande.rajkiran@gmail.com>
2019-12-06 17:50:51 +0530

    [EKA-]Refactor gradle files to remove boilerplate code from all modules

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,49 @@
+import org.gradle.api.JavaVersion
+
+object Config {
+    const val applicationId = "in.org.projecteka.jataayu"
+    const val compileSdkVersion = 9
+    const val minSdkVersion = 23
+    const val buildToolsVersion = "29.0.2"
+    val javaVersion = JavaVersion.VERSION_1_8
+}
+
+object LibVersions {
+    const val appcompat = "1.1.0"
+    const val coreKtx = "1.1.0"
+    const val androidX = "1.1.1"
+    const val jUnit = "4.12"
+    const val espresso = "3.2.0"
+    const val jacoco = "0.8.5"
+    const val koin = "2.0.1"
+    const val kotlin = "1.3.61"
+    const val support = "28.0.0"
+    const val constraintLayout = "1.1.3"
+    const val retrofit = "2.5.0"
+    const val gradle = "0.21.0"
+    const val androidStudio = "3.5.2"
+    const val dicemelonJacoco = "0.1.4"
+}
+
+object Deps {
+    const val appcompat = "androidx.appcompat:appcompat:${LibVersions.appcompat}"
+    const val coreKtx = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${LibVersions.kotlin}"
+    const val androidX = "androidx.core:core-ktx:${LibVersions.androidX}"
+    const val koinAndroid = "org.koin:koin-android:${LibVersions.koin}"
+    const val koinAndroidXViewModel = "org.koin:koin-androidx-viewmodel:${LibVersions.koin}"
+    const val koinAndroidXScope = "org.koin:koin-androidx-scope:${LibVersions.koin}"
+
+    const val jUnit = "junit:junit:${LibVersions.jUnit}"
+    const val koinText = "org.koin:koin-test:${LibVersions.koin}"
+
+    const val androidXTest = "androidx.test.ext:junit:${LibVersions.androidX}"
+    const val espresso = "androidx.test.espresso:espresso-core:${LibVersions.espresso}"
+    const val toolsGradleVersions = "com.github.ben-manes:gradle-versions-plugin:${LibVersions.gradle}"
+
+    const val toolsAndroidStudio = "com.android.tools.build:gradle:${LibVersions.androidStudio}"
+    const val toolsKotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${LibVersions.kotlin}"
+    const val toolsKotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${LibVersions.kotlin}"
+    const val toolsDicemelonJacoco = "com.dicedmelon.gradle:jacoco-android:${LibVersions.dicemelonJacoco}"
+    const val toolsJacocoCore = "org.jacoco:org.jacoco.core:${LibVersions.jacoco}"
+    const val toolsJacocoAgent = "org.jacoco:org.jacoco.agent:${LibVersions.jacoco}"
+}
