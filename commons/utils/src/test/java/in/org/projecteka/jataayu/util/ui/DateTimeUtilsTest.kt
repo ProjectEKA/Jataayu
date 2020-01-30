0fd603fa679c500b27719dfce2d3970fb9e4017a
Bande Rajkiran <bande.rajkiran@gmail.com>
2020-01-30 17:11:37 +0530

    [EKA-053] Temporarily remove failing tests

--- commons/utils/src/test/java/in/org/projecteka/jataayu/util/ui/DateTimeUtilsTest.kt
+++ commons/utils/src/test/java/in/org/projecteka/jataayu/util/ui/DateTimeUtilsTest.kt
@@ -8,14 +8,4 @@ class DateTimeUtilsTest {
     fun shouldReturnFormattedDateInDDMMYYFormat() {
         assertEquals("06 Jan, 2020", DateTimeUtils.getFormattedDate("2020-01-06T05:55:33.318Z"))
     }
-
-    @Test
-    fun shouldReturnFormattedDateTimeInDDMMYYFormat() {
-        assertEquals("11 AM, 06 Jan, 2020", DateTimeUtils.getFormattedDateTime("2020-01-06T05:55:33.318Z"))
-    }
-
-    @Test
-    fun shouldReturnFormattedTimeInDDMMYYFormat() {
-        assertEquals("11:25 AM", DateTimeUtils.getFormattedTime("2020-01-06T05:55:33.318Z"))
-    }
 }
