6e54595089b25ea026b10c6f6bfc7001fb0329c4
Bande Rajkiran <bande.rajkiran@gmail.com>
2020-03-05 16:36:06 +0530

    [EKA-000] Add hack to bypass HTTPS handshake exception issue

--- commons/network/src/main/java/in/projecteka/jataayu/network/NetworkManager.kt
+++ commons/network/src/main/java/in/projecteka/jataayu/network/NetworkManager.kt
@@ -15,7 +15,14 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 fun createNetworkClient(context: Application, debug: Boolean = false) =
     retrofitClient(getBaseUrl(context), httpClient(debug, context, context.getAuthToken()))
@@ -25,19 +32,50 @@ private fun getBaseUrl(context: Application): String {
 }
 
 private fun httpClient(debug: Boolean, context: Context, authToken: String): OkHttpClient {
+
     val httpLoggingInterceptor = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger.DEFAULT)
+    // Create a trust manager that does not validate certificate chains
+    val trustAllCerts: Array<TrustManager> = arrayOf<TrustManager>(
+        object : X509TrustManager {
+            @Throws(CertificateException::class)
+            override fun checkClientTrusted(
+                chain: Array<X509Certificate?>?,
+                authType: String?
+            ) {
+            }
+
+            @Throws(CertificateException::class)
+            override fun checkServerTrusted(
+                chain: Array<X509Certificate?>?,
+                authType: String?
+            ) {
+            }
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> {
+                return arrayOf()
+            }
+        }
+    )
+    // Install the all-trusting trust manager
+    // Install the all-trusting trust manager
+    val sslContext: SSLContext = SSLContext.getInstance("SSL")
+    sslContext.init(null, trustAllCerts, SecureRandom())
+    // Create an ssl socket factory with our all-trusting manager
+    val sslSocketFactory = sslContext.socketFactory
     val clientBuilder = OkHttpClient.Builder()
+    clientBuilder.sslSocketFactory(
+        sslSocketFactory,
+        trustAllCerts[0] as X509TrustManager
+    )
+    clientBuilder.hostnameVerifier(HostnameVerifier { hostname, session -> true })
     clientBuilder.connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
         .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
         .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
-
     clientBuilder.addInterceptor(RequestInterceptor(authToken))
-
     if (debug && !isTestingMode(context)) {
         addRequestResponseLogger(httpLoggingInterceptor, clientBuilder)
         addBaseUrlChanger(clientBuilder, context)
     }
-
     return clientBuilder.build()
 }
 
