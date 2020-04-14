518321d16387a4640cfd8d1a16c07ab1a58bfacc
mabu <mahabaleshwar.hegde@thoughtworks.comcd>
2020-04-14 14:54:08 +0530

    [EKA-000] CM_APP 12 reverted userName key to username in create registration payload. since backend fixed this issue

--- features/core/src/main/java/in/projecteka/jataayu/core/model/CreateAccountRequest.kt
+++ features/core/src/main/java/in/projecteka/jataayu/core/model/CreateAccountRequest.kt
@@ -3,7 +3,7 @@ package `in`.projecteka.jataayu.core.model
 import com.google.gson.annotations.SerializedName
 
 data class CreateAccountRequest(
-    @SerializedName("userName") val userName: String,
+    @SerializedName("username") val userName: String,
     @SerializedName("password") val password: String,
     @SerializedName("name") val name: String,
     @SerializedName("gender") val gender: String,
