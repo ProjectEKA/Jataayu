7f98767f568535197093c3cc7e0989b4c7a83120
Krupa <krupa.m.desai@gmail.com>
2020-05-12 11:24:54 +0530

    [EKA-000]CM_APP_22:Updated class name

--- app/src/androidTest/java/in/projecteka/jataayu/consent/ui/fragment/LoginActivityTest.kt
+++ app/src/androidTest/java/in/projecteka/jataayu/consent/ui/fragment/LoginActivityTest.kt
@@ -15,7 +15,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class LoginFragmentTest{
+class LoginActivityTest{
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(LoginActivity::class.java)
