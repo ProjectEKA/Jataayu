e9958e494935cd63f3dc93da8203435f9e791833
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-05-22 10:50:20 +0530

    [EKA-044] | Dheeraj/Momin | Fix tests for CreateAccountViewModel

--- user/account/src/test/java/in/projecteka/jataayu/user/account/viewmodel/CreateAccountViewModelTest.kt
+++ user/account/src/test/java/in/projecteka/jataayu/user/account/viewmodel/CreateAccountViewModelTest.kt
@@ -1,29 +1,19 @@
 package `in`.projecteka.jataayu.user.account.viewmodel
 
-import `in`.projecteka.jataayu.core.model.CreateAccountRequest
 import `in`.projecteka.jataayu.core.model.CreateAccountResponse
-import `in`.projecteka.jataayu.network.utils.Success
 import `in`.projecteka.jataayu.core.repository.UserAccountsRepository
-import `in`.projecteka.jataayu.util.TestUtils
-import `in`.projecteka.jataayu.util.extension.fromJson
-import `in`.projecteka.jataayu.util.repository.CredentialsRepository
-import `in`.projecteka.jataayu.util.repository.PreferenceRepository
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.google.gson.Gson
 import junit.framework.Assert.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 @RunWith(MockitoJUnitRunner::class)
 class CreateAccountViewModelTest {
@@ -33,20 +23,12 @@ class CreateAccountViewModelTest {
     @Mock
     private lateinit var repository: UserAccountsRepository
 
-    @Mock
-    private lateinit var preferenceRepository: PreferenceRepository
-
-    @Mock
-    private lateinit var credentialsRepository: CredentialsRepository
-
     @get:Rule
     val taskExecutorRule = InstantTaskExecutorRule()
 
     @Mock
     private lateinit var createAccountAccountCall: Call<CreateAccountResponse>
 
-    @Mock
-    private lateinit var responseCallback: Callback<CreateAccountResponse>
 
     @Before
     fun setup() {
@@ -84,8 +66,6 @@ class CreateAccountViewModelTest {
     @Test
     fun `should enable submit button when all the required fields entered`() {
 
-        viewModel.inputUsernameLbl.set("raj1")
-        viewModel.inputPasswordLbl.set("Raj1234@13")
         viewModel.inputFullName.set("mabu")
         viewModel.onCheckedChanged(null, 2)
         assertEquals(true,  viewModel.validateFields())
@@ -95,8 +75,6 @@ class CreateAccountViewModelTest {
     @Test
     fun `should disable submit button when gender not selected`() {
 
-        viewModel.inputUsernameLbl.set("raj1")
-        viewModel.inputPasswordLbl.set("Raj1234@13")
         viewModel.inputFullName.set("mabu")
         assertEquals(false,  viewModel.validateFields())
         assertEquals(false, viewModel.submitEnabled.get())
@@ -105,8 +83,6 @@ class CreateAccountViewModelTest {
     @Test
     fun `should disable submit button when name not entered`() {
 
-        viewModel.inputUsernameLbl.set("raj1")
-        viewModel.inputPasswordLbl.set("Raj1234@13")
         viewModel.inputFullName.set("")
         assertEquals(false,  viewModel.validateFields())
         assertEquals(false, viewModel.submitEnabled.get())
@@ -114,9 +90,6 @@ class CreateAccountViewModelTest {
 
     @Test
     fun `should disable submit button when username is not entered`() {
-
-        viewModel.inputUsernameLbl.set("")
-        viewModel.inputPasswordLbl.set("Raj1234@13")
         viewModel.inputFullName.set("mabu")
         assertEquals(false,  viewModel.validateFields())
         assertEquals(false, viewModel.submitEnabled.get())
@@ -124,31 +97,9 @@ class CreateAccountViewModelTest {
 
     @Test
     fun `should disable submit button when password is not entered`() {
-        viewModel.inputUsernameLbl.set("raj2904201")
-        viewModel.inputPasswordLbl.set("")
         viewModel.inputFullName.set("maabu")
         assertEquals(false,  viewModel.validateFields())
         assertEquals(false, viewModel.submitEnabled.get())
-    }
-
-    @Test
-    fun `should create correct create account payload for entered inputs`() {
-        val username = "raj-bande1"
-        val password = "Vik2704"
-        val fullName = "Maabu"
-        val provider = "hegde"
-        viewModel.usernameProviderLbl.set(provider)
-        viewModel.inputUsernameLbl.set(username)
-        viewModel.inputPasswordLbl.set(password)
-        viewModel.inputFullName.set(fullName)
-        viewModel.selectedYoB(1977)
-        viewModel.onCheckedChanged(null, 2)
-        val createAccountRequest = viewModel.getCreateAccountPayload()
-        assertEquals(username + provider, createAccountRequest.userName)
-        assertEquals(password, createAccountRequest.password)
-        assertEquals(fullName, createAccountRequest.name)
-        assertEquals("O", createAccountRequest.gender)
-        assertEquals(1977, createAccountRequest.yearOfBirth)
     }
 
     @Test
@@ -187,28 +138,6 @@ class CreateAccountViewModelTest {
         assertFalse(!viewModel.showErrorAyushmanId.get())
     }
 
-
-    @Test
-    fun `should create correct create account payload for entered inputs with ayushmanBharatID`() {
-        val username = "raj-bande1"
-        val password = "Vik2704"
-        val fullName = "Maabu"
-        val provider = "hegde"
-        val bharatId = "PAYUSh123"
-        viewModel.usernameProviderLbl.set(provider)
-        viewModel.inputUsernameLbl.set(username)
-        viewModel.inputPasswordLbl.set(password)
-        viewModel.inputFullName.set(fullName)
-        viewModel.inputAyushmanIdLbl.set(bharatId)
-        viewModel.selectedYoB(1977)
-        viewModel.onCheckedChanged(null, 2)
-        val createAccountRequest = viewModel.getCreateAccountPayload()
-        assertEquals(username + provider, createAccountRequest.userName)
-        assertEquals(password, createAccountRequest.password)
-        assertEquals(fullName, createAccountRequest.name)
-        assertEquals("O", createAccountRequest.gender)
-        assertEquals(1977, createAccountRequest.yearOfBirth)
-    }
 
     @Test
     fun `test years to populate of list of 120 years`() {
