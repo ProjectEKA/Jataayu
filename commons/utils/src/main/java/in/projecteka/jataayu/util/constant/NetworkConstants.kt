b4282f9509a9bead429eaffbc7534facad3531bb
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2020-07-09 11:26:01 +0530

    [CM_APP_628] Configure base url as the build config field

--- commons/utils/src/main/java/in/projecteka/jataayu/util/constant/NetworkConstants.kt
+++ commons/utils/src/main/java/in/projecteka/jataayu/util/constant/NetworkConstants.kt
@@ -1,8 +1,10 @@
 package `in`.projecteka.jataayu.util.constant
 
+import `in`.projecteka.jataayu.util.BuildConfig
+
 class NetworkConstants {
     companion object {
-        const val PROD_URL : String = "https://ncg-dev.projecteka.in/consent-manager/"
+        var PROD_URL : String = BuildConfig.PROD_URL
         const val TEST_URL : String = "http://10.0.2.2:8000/"   
             const val MOCKOON_URL : String = "http://10.0.2.2:3000/"
         const val MOCK_WEB_SERVER_TEST_URL : String = "http://localhost:8080"
